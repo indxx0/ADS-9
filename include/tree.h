f4adf037c4f78f61aaa68628467a7a856cc3c4cf
indx0 <111313725+indxx0@users.noreply.github.com>
2026-06-07 18:59:14 +0300

    Implement PMTree class for permutation generation
    Added PMTree class with Node structure and methods for permutations.

--- include/tree.h
+++ include/tree.h
@@ -1,5 +1,28 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
+#include <vector>
 
+class PMTree {
+public:
+    struct Node {
+        char value;
+        std::vector<Node*> children;
+        Node(char v) : value(v) {}
+    };
+
+    Node* root;
+    int depth;
+
+    PMTree(std::vector<char> in);
+    ~PMTree();
+
+private:
+    void build(Node* node, std::vector<char>& avail);
+    void destroy(Node* node);
+};
+
+std::vector<std::vector<char>> getAllPerms(const PMTree& tree);
+std::vector<char> getPerm1(const PMTree& tree, int num);
+std::vector<char> getPerm2(const PMTree& tree, int num);
 #endif  // INCLUDE_TREE_H_
